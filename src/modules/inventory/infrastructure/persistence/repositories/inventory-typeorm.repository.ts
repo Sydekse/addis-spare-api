eeb46455aefaca81b96c1b6598647a824456e886
coleYab <yabume123@gmail.com>
2025-07-08 13:01:59 +0000

    fix: added some missing inventory repository methods

--- src/modules/inventory/infrastructure/persistence/repositories/inventory-typeorm.repository.ts
+++ src/modules/inventory/infrastructure/persistence/repositories/inventory-typeorm.repository.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
 import { InventoryRepository } from '../../../domain/repositories/inventory.repository';
 import { InventoryTypeOrmEntity } from '../typeorm/inventory-typeorm.entity';
 import { Inventory } from 'src/modules/inventory/domain/entities/inventory.entity';
+import { OrderInventory } from 'src/modules/order/infrastructure/persistence/typeorm/order-typeorm.entity';
 
 @Injectable()
 export class InventoryTypeOrmRepository implements InventoryRepository {
@@ -11,6 +16,74 @@ export class InventoryTypeOrmRepository implements InventoryRepository {
     @InjectRepository(InventoryTypeOrmEntity)
     private readonly repository: Repository<InventoryTypeOrmEntity>,
   ) {}
+  async restoreDeductedInventories(
+    orderInventories: OrderInventory[],
+    manager: EntityManager,
+  ): Promise<void> {
+    for (const orderInventory of orderInventories) {
+      const inventory = await this.repository.findOne({
+        where: { id: orderInventory.id },
+      });
+      if (!inventory) {
+        throw new NotFoundException(
+          `inventory with id=${orderInventory.id} is not found`,
+        );
+      }
+
+      inventory.quantity += orderInventory.quantity;
+      await manager.save(inventory);
+    }
+  }
+
+  async deductQuantityForProduct(
+    productId: string,
+    requiredQuantity: number,
+    manager: EntityManager,
+  ): Promise<OrderInventory[]> {
+    const inventories = await this.repository.find({
+      where: { productId },
+      order: { quantity: 'DESC' },
+    });
+    const orderInventories: OrderInventory[] = [];
+
+    let remaining = requiredQuantity;
+    for (const inventory of inventories) {
+      if (remaining <= 0) break;
+      const deduct = Math.min(remaining, inventory.quantity);
+      inventory.quantity -= deduct;
+      remaining -= deduct;
+      if (deduct != 0) {
+        orderInventories.push(new OrderInventory(inventory.id, deduct));
+      }
+      await manager.save(InventoryTypeOrmEntity, inventory);
+    }
+
+    if (remaining > 0) {
+      throw new BadRequestException(
+        `not enough inventory for the product with id=${productId}`,
+      );
+    }
+
+    return orderInventories;
+  }
+
+  async findInventoriesByProductId(id: string): Promise<Inventory[]> {
+    const inventories = await this.repository.find({
+      where: { productId: id },
+      order: { quantity: 'DESC' },
+    });
+    return inventories.map(
+      (entity) =>
+        new Inventory(
+          entity.id,
+          entity.productId,
+          entity.location,
+          entity.quantity,
+          entity.reorderThreshold,
+          entity.supplierId,
+        ),
+    );
+  }
 
   async findById(id: string): Promise<Inventory | null> {
     const entity = await this.repository.findOne({ where: { id } });
